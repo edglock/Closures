a0f0ea42bf17d16333551b2767d4b59754680a19
Edward <50621180+edglock@users.noreply.github.com>
2020-05-02 23:14:37 +0300

    developed

--- js/index.js
+++ js/index.js
@@ -1,12 +1,27 @@
 function logPerson() {
     console.log(`Person: ${this.name}, ${this.age}, ${this.job}`);
 }
-const person1 = {name: 'Edward', age: 47, job: student};
-function bind(context, func) {
-    const name = context.name;
-    const age = context.age;
-    const job = context.job;
+const person1 = {name: 'Edward', age: 47, job: 'student'};
+function bind(context, fanc) {
     return function() {
-        
+        fanc.apply(context);
     }
 }
+bind(person1, logPerson)();
+
+function add(first) {
+    return function(second) {
+        console.log(first + second);
+    }
+}
+const calc = add(10);
+calc(20);
+
+function pow(base, exponent) {
+    if ( exponent === 0) {
+        return 1;
+    } else {
+        return base * pow(base, exponent - 1);
+    }
+}
+console.log(pow(2, 3));
